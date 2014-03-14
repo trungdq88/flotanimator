7d3b1956830b0261afa8171544f5422cfccc8d06
unknown <TrungDQ3@311TrungDQ3-W7.fsoft.fpt.vn>
2014-03-14 09:44:35 +0700

    Update support multiple series

--- jquery.flot.animator.js
+++ jquery.flot.animator.js
@@ -1,131 +1,189 @@
 /* jQuery Flot Animator version 1.0.
 
-Flot Animator is a free jQuery Plugin that will add fluid animations to Flot charts.
+ Flot Animator is a free jQuery Plugin that will add fluid animations to Flot charts.
 
-Copyright (c) 2012-2013 Chtiwi Malek
-http://www.codicode.com/art/jquery_flot_animator.aspx
+ Copyright (c) 2012-2013 Chtiwi Malek
+ http://www.codicode.com/art/jquery_flot_animator.aspx
 
-Licensed under Creative Commons Attribution 3.0 Unported License.
-*/
+ Modified by Dinh Quang Trung (2014): support animate multiple series.
+
+ Licensed under Creative Commons Attribution 3.0 Unported License.
+ */
 
 $.extend({
-  plotAnimator: function (chart, data,g){
-    
-    var serie = 0;
-    for (var i = 0; i < data.length; i++)
-    {
-      if (data[i].animator)
-      {
-        serie = i;
-      }
-    }
-    
-    function pInit(arr){
-	  var x = [];
-      x.push([arr[0][0], Math.max.apply(Math, arr.map(function(i) { return i[1];}))]);
-      x.push([arr[0][0], null]);
-      x.push([arr[0][0], Math.min.apply(Math, arr.map(function(i) { return i[1];}))]);
-      for(var i = 0; i < arr.length; i++) {
-          x.push([arr[i][0], null]);
-      }
-      data[serie].data = x;
-      return $.plot(chart, data, g);
-    }
-    
-    var d0 = data[serie];
-    var oData = d0.data;
-    
-    var plot = pInit(oData);
-    
-    var isLines = (data[serie].lines)?true:false;
-    var steps = (data[serie].animator && data[serie].animator.steps) || 135;
-    var duration = (data[serie].animator && data[serie].animator.duration) || 1000;
-    var start = (data[serie].animator && data[serie].animator.start) || 0;
-    var dir = (data[serie].animator && data[serie].animator.direction) || "right";
-    function stepData()
-    {
-      var Si = oData[0][0];
-      var Fi = oData[oData.length-1][0];
-      var Pas = (Fi-Si)/steps;
-      
-      var d2 = [];      
-      d2.push(oData[0]);
-      var nPointPos = 1;
-      lPoint = oData[0];
-      nPoint = oData[nPointPos];
-      for (var i = Si+Pas; i < Fi+Pas; i += Pas)
-      {
-        if (i>Fi) {i=Fi;}
-        $("#m2").html(i);
-        while (i > nPoint[0])
-        {
-          lPoint = nPoint;
-          nPoint = oData[nPointPos++];
+    plotAnimator: function (chart, data, g) {
+
+        var plot;
+
+        // Original data
+        var oData = [];
+
+        // Contains `data`  elements index, which are animated
+        var series = [];
+
+        // All series animation profile
+        var seriesProfile = [];
+
+        // Read series with animate profile
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].animator) {
+                series.push(i);
+            }
         }
-        if (i == nPoint[0])
-        {
-          d2.push([i,nPoint[1]]);
-          lPoint = nPoint;
-          nPoint = oData[nPointPos++];
+
+        // If no serie is selected, then animate all
+        if (series.length == 0) {
+            for (var i = 0; i < data.length; i++) {
+                series.push(i);
+            }
         }
-        else
-        {
-          var a = ((nPoint[1]-lPoint[1]) / ((nPoint[0]-lPoint[0])));
-          curV = (a * i) + (lPoint[1] - (a * lPoint[0]));
-          d2.push([i,curV]);
+
+
+        function pInit(arr, serie) {
+            var x = [];
+            x.push([arr[0][0], Math.max.apply(Math, arr.map(function (i) {
+                return i[1];
+            }))]);
+            x.push([arr[0][0], null]);
+            x.push([arr[0][0], Math.min.apply(Math, arr.map(function (i) {
+                return i[1];
+            }))]);
+            for (var i = 0; i < arr.length; i++) {
+                x.push([arr[i][0], null]);
+            }
+            data[serie].data = x;
         }
-      }
-      return d2;
+
+
+
+        // Prepare data for base drawing and save series profiles
+        for (var i = 0; i < series.length; ++i) {
+            var serie = series[i];
+
+            var d0 = data[serie];
+            oData[serie] = d0.data;
+
+            pInit(oData[serie], serie);
+
+            seriesProfile.push({
+                index: serie,
+                isLines: (data[serie].lines) ? true : false,
+                steps: (data[serie].animator && data[serie].animator.steps) || 500,
+                duration: (data[serie].animator && data[serie].animator.duration) || 2000,
+                start: (data[serie].animator && data[serie].animator.start) || 0,
+                dir: (data[serie].animator && data[serie].animator.direction) || "right"
+            });
+        }
+
+        // Draw base
+        plot = $.plot(chart, data, g);
+
+        // Calc step data from profile
+        for (var i = 0; i < seriesProfile.length; ++i) {
+            var steps = seriesProfile[i].steps;
+            seriesProfile[i].sData = stepData(oData[seriesProfile[i].index], steps);
+        }
+
+
+        /**
+         * TODO: Make this function more abstract:
+         * TODO: Get step data of a serial
+         * @returns {Array}
+         */
+        function stepData(_oData, steps) {
+            // Get config
+            var Si = _oData[0][0];
+            var Fi = _oData[_oData.length - 1][0];
+            var Pas = (Fi - Si) / steps;
+
+            // Define return variable
+            var d2 = [];
+
+            // Calculate data
+            d2.push(_oData[0]);
+            var nPointPos = 1;
+            lPoint = _oData[0];
+            nPoint = _oData[nPointPos];
+            for (var i = Si + Pas; i < Fi + Pas; i += Pas) {
+                if (i > Fi) {
+                    i = Fi;
+                }
+                $("#m2").html(i);
+                while (i > nPoint[0]) {
+                    lPoint = nPoint;
+                    nPoint = _oData[nPointPos++];
+                }
+                if (i == nPoint[0]) {
+                    d2.push([i, nPoint[1]]);
+                    lPoint = nPoint;
+                    nPoint = _oData[nPointPos++];
+                }
+                else {
+                    var a = ((nPoint[1] - lPoint[1]) / ((nPoint[0] - lPoint[0])));
+                    curV = (a * i) + (lPoint[1] - (a * lPoint[0]));
+                    d2.push([i, curV]);
+                }
+            }
+            return d2;
+        }
+
+        // One step for all
+        var step = 0;
+
+        function plotData() {
+
+            // Data holder (data to be drawn)
+            step++;
+
+            for (var i = 0; i < seriesProfile.length; ++i) {
+                var d3 = [];
+                switch (seriesProfile[i].dir) {
+                    case 'right':
+                        d3 = seriesProfile[i].sData.slice(0, step);
+                        break;
+                    case 'left':
+                        d3 = seriesProfile[i].sData.slice(-1 * step);
+                        break
+                    case 'center':
+                        d3 = seriesProfile[i].sData.slice((seriesProfile[i].sData.length / 2) - (step / 2), (seriesProfile[i].sData.length / 2) + (step / 2));
+                        break;
+                }
+
+                // TODO: fix for non-line series
+                if (!seriesProfile[i].isLines) {
+                    inV = d3[0][0];
+                    laV = d3[d3.length - 1][0];
+                    d3 = [];
+
+                    var d0 = data[seriesProfile[i].index];
+                    var oData = d0.data;
+
+                    for (var j = 0; j < oData.length; j++) {
+                        if (oData[j][0] >= inV && oData[j][0] <= laV) {
+                            d3.push(oData[j]);
+                        }
+                    }
+                }
+//                data[seriesProfile[i].index].data = (step < seriesProfile[i].steps) ? d3 : oData[seriesProfile[i].index];
+                data[seriesProfile[i].index].data = d3;
+
+            } // End for
+
+
+            // TODO: currently the duration only will apply the first serial profile only. Let's make it more useful!
+            plot.setData(data);
+            plot.draw();
+
+            // Do a loop, or stop
+            if (step < seriesProfile[0].steps) {
+                setTimeout(plotData, seriesProfile[0].duration / seriesProfile[0].steps);
+            }
+            else {
+                // chart.trigger("animatorComplete");
+            }
+        }
+
+        setTimeout(plotData, 500);
+        return plot;
     }
-    
-    var step=0;
-    var sData = stepData();
-    function plotData()
-    {
-      var d3=[];
-      step++;
-      
-      switch(dir)
-      {
-        case 'right':
-          d3 = sData.slice(0, step);
-          break;
-        case 'left':
-          d3 = sData.slice(-1*step);
-          break
-          case 'center':
-          d3 = sData.slice((sData.length/2)-(step/2),(sData.length/2)+(step/2));
-          break;
-      }
-      
-      if (!isLines)
-      {
-        inV = d3[0][0];
-      	laV = d3[d3.length-1][0];
-        d3=[];
-        for (var i = 0; i < oData.length; i++)
-      	{
-          if (oData[i][0]>=inV && oData[i][0]<=laV)
-          {
-            d3.push(oData[i]);
-          }
-      	}
-      }
-      
-      data[serie].data = (step<steps)?d3:oData;
-      plot.setData(data);
-      plot.draw();
-      if (step<steps)
-      {
-        setTimeout(plotData, duration/steps);
-      }
-      else
-      {
-        chart.trigger( "animatorComplete" );
-      }
-    }
-    
-    setTimeout(plotData,start);
-    return plot;
-  }
 });
